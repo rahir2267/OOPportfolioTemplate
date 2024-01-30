4c9e09364d364a11b3746b16fbe543d42129ae39
c7361886 <R.Ahir2267@student.leedsbeckett.ac.uk>
2024-01-30 11:25:50 +0000

    commit

---
+++ WeeklyExercises/1GettingStarted/Diamond.java
@@ -0,0 +1,25 @@
+public class Diamond {
+
+    public static void main(String[] args) {
+        int n = 4;
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++)
+                System.out.print(" ");
+            for (int k = 0; k <= i * 2; k++)
+                System.out.print("*");
+            System.out.println();
+        }
+
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n - i; j++)
+                System.out.print(" ");
+            for (int k = 0; k <= i * 2; k++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
+
