5fc2a00893091399b089e74b61a80169fc2ba408
c7361886 <R.Ahir2267@student.leedsbeckett.ac.uk>
2024-01-30 11:17:55 +0000

    commit

---
+++ WeeklyExercises/1GettingStarted/PersonalDetails.java
@@ -0,0 +1,15 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+
+        String name = "Raj Ahir";
+        String address = "6 Ash crescent Leeds";
+        int age = 21;
+        String phoneNumber = "07771023293";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Address: " + address);
+        System.out.println("Phone Number: " + phoneNumber);
+
+    }
+}
